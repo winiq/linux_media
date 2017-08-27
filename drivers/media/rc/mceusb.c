3e77a1d9d8dfb730d593f9cc96045deba927ec2a
tako035 <tkocakurt@gmail.com>
2017-08-28 02:59:07 +0300

    Update mceusb.c
    Add support for builtin mceusb support for TBS5990

--- drivers/media/rc/mceusb.c
+++ drivers/media/rc/mceusb.c
@@ -189,6 +189,8 @@ enum mceusb_model_type {
 	MCE_GEN2_NO_TX,
 	HAUPPAUGE_CX_HYBRID_TV,
 	CX231XX_TBS5280,
+        CX231XX_TBS5281,
+        CX231XX_TBS5990,
 };
 
 struct mceusb_model {
@@ -252,6 +254,14 @@ static const struct mceusb_model mceusb_model[] = {
 		.rc_map = RC_MAP_RC6_MCE,
 		.name = "TurboSight TBS 5280 (cx231xx) MCE IR",
 	},
+        [CX231XX_TBS5281] = {
+                .rc_map = RC_MAP_RC6_MCE,
+                .name = "TurboSight TBS 5281 (cx231xx) MCE IR",
+        },
+        [CX231XX_TBS5990] = {
+                .rc_map = RC_MAP_RC6_MCE,
+                .name = "TurboSight TBS 5990 (cx231xx) MCE IR",
+        },
 };
 
 static struct usb_device_id mceusb_dev_table[] = {
@@ -403,7 +413,15 @@ static struct usb_device_id mceusb_dev_table[] = {
 	  .driver_info = HAUPPAUGE_CX_HYBRID_TV },
 	/* Adaptec / HP eHome Receiver */
 	{ USB_DEVICE(VENDOR_ADAPTEC, 0x0094) },
-
+        /* TurboSight TBS 5280 IR */
+        { USB_DEVICE(0x734c, 0x5280),
+          .driver_info = CX231XX_TBS5280 },
+        /* TurboSight TBS 5281 IR */
+        { USB_DEVICE(0x734c, 0x5281),
+          .driver_info = CX231XX_TBS5281 },
+        /* TurboSight TBS 5990 IR */
+        { USB_DEVICE(0x734c, 0x5990),
+          .driver_info = CX231XX_TBS5990 },
 	/* Terminating entry */
 	{ }
 };
@@ -1254,7 +1272,7 @@ static struct rc_dev *mceusb_init_rc_dev(struct mceusb_dev *ir)
 	struct rc_dev *rc;
 	int ret;
 
-	rc = rc_allocate_device(RC_DRIVER_IR_RAW);
+	rc = rc_allocate_device(RC_DRIVER_SCANCODE);
 	if (!rc) {
 		dev_err(dev, "remote dev allocation failed");
 		goto out;
@@ -1274,7 +1292,7 @@ static struct rc_dev *mceusb_init_rc_dev(struct mceusb_dev *ir)
 	usb_to_input_id(ir->usbdev, &rc->input_id);
 	rc->dev.parent = dev;
 	rc->priv = ir;
-	rc->allowed_protocols = RC_BIT_ALL_IR_DECODER;
+	rc->allowed_protocols = RC_BIT_ALL;
 	rc->timeout = MS_TO_NS(100);
 	if (!ir->flags.no_tx) {
 		rc->s_tx_mask = mceusb_set_tx_mask;
