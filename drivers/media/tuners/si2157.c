d987975d42f0f9c87f32cdaf7cd0707ad127c805
CrazyCat <crazycat69@narod.ru>
2019-09-14 19:33:52 +0300

    si2157: Update tuner wakeup on probe.

--- drivers/media/tuners/si2157.c
+++ drivers/media/tuners/si2157.c
@@ -452,8 +452,22 @@ static int si2157_probe(struct i2c_client *client,
 	INIT_DELAYED_WORK(&dev->stat_work, si2157_stat_work);
 
 	/* check if the tuner is there */
-	cmd.wlen = 0;
-        cmd.rlen = 1;
+	/* wake tuner */
+	switch (dev->chiptype)
+	{
+	case SI2157_CHIPTYPE_SI2141:
+		memcpy(cmd.args, "\xc0\x08\x01\x02\x00\x08\x01", 7);
+		cmd.wlen = 7;
+		break;
+	case SI2157_CHIPTYPE_SI2146:
+		memcpy(cmd.args, "\xc0\x05\x01\x00\x00\x0b\x00\x00\x01", 9);
+		cmd.wlen = 9;
+		break;
+	default:
+		memcpy(cmd.args, "\xc0\x00\x0c\x00\x00\x01\x01\x01\x01\x01\x01\x02\x00\x00\x01", 15);
+		cmd.wlen = 15;
+	}
+	cmd.rlen = 1;
 	ret = si2157_cmd_execute(client, &cmd);
 	if (ret)
 		goto err_kfree;
